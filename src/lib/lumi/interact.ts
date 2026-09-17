955ccf303758caae6db4059f7df7fd4f2cd18939
RBX2042 <125316921+RBX2042@users.noreply.github.com>
2026-09-17 13:38:51 +0700

    Herstel interact.ts en koppel arcade-soorten

--- src/lib/lumi/interact.ts
+++ src/lib/lumi/interact.ts
@@ -20,3 +20,21 @@ import type {
 import { groupIndex } from "./types.ts";
 
 export { ballon, regen, sprint } from "./arcade.ts";
+
+function rand(n: number): number {
+  return Math.floor(Math.random() * n);
+}
+function pick<T>(arr: readonly T[]): T {
+  return arr[rand(arr.length)]!;
+}
+function shuffle<T>(arr: T[]): T[] {
+  const a = [...arr];
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = rand(i + 1);
+    [a[i], a[j]] = [a[j]!, a[i]!];
+  }
+  return a;
+}
+function between(min: number, max: number): number {
+  return min + rand(max - min + 1);
+}
